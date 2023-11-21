00c2a3427db48e86c23f8607fd8ef730ceda85b1
Antstar609 <antstar666@gmail.com>
2023-11-21 16:42:08 +0100

    Changes on test.lua

--- data/scripts/test.lua
+++ data/scripts/test.lua
@@ -1,1 +1,9 @@
-System.LogAlways("Hello World ///////////////////////////////////////////////// TestMod !")
+-- System.LogAlways("Hello World ///////////////////////////////////////////////// TestMod !")
+
+local Entity = { x = 0, y = 0 }
+
+for i = 1, 10, 1 do
+	local Pos = {math.random(1, 100), math.random(1, 100)}
+	table.insert(Entity, Pos)
+	print("Entity[" .. i .. "] = {" .. Pos[1] .. ", " .. Pos[2] .. "}")
+end

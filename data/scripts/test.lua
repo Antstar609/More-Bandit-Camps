4d121e0d90289e47b7da357d749d8133cf83cc14
Antstar609 <antstar666@gmail.com>
2023-12-06 23:46:03 +0100

    Add 'spawnEntity' function

--- data/scripts/test.lua
+++ data/scripts/test.lua
@@ -41,10 +41,26 @@ System.AddCCommand('printText', 'printText()', "Print text to the screen")
 
 function listEntities()
 	center = System.GetEntityByName("dude"):GetWorldPos()
-	entities = System.GetEntities(center, 5)
+	entities = System.GetEntities(center, 2)
 	for i, entity in ipairs(entities) do
 		System.LogAlways(entity:GetName())
 	end
 end
 
 System.AddCCommand('listEntities', 'listEntities()', "List entities in a sphere")
+
+function spawnEntity()
+    local entityParams = {
+        class = "NPC_Female", 
+        name = "MyEntity",
+        position = System.GetEntityByName("dude"):GetWorldPos()
+    }
+    local newEntity = System.SpawnEntity(entityParams)
+    if newEntity then
+        System.LogAlways("Entity spawned: " .. newEntity:GetName())
+    else  
+        System.LogAlways("Failed to spawn entity")
+    end
+end
+
+System.AddCCommand('spawnEntity', 'spawnEntity()', "Spawns an entity")

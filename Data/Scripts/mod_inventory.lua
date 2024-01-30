d784f93dd4bdafe080b03b4a330a05fe3c89d55e
Antstar609 <antstar666@gmail.com>
2024-01-30 10:26:58 +0100

    Comment non-working code

--- Data/Scripts/mod_inventory.lua
+++ Data/Scripts/mod_inventory.lua
@@ -5,18 +5,21 @@ ModInventory = {}
 function ModInventory:DumpPlayerInventory()
 	local count = player.inventory:GetCount()
 	ModUtils:Log("Inventory count: " .. count)
-	
+
 	Database.LoadTable("item")
 	local tableData = Database.GetTableInfo("item")
 	local rows = tableData.LineCount - 1
-	
+
 	for i = 0, rows do
 		local lineInfo = Database.GetTableLine("item", i)
-		local itemID = lineInfo.item_id
-		--TODO: HasItem() doesn't work for some reason
-		if (player.inventory.HasItem(itemID)) then
-			ModUtils:Log("Item: " .. itemID .. " (" .. lineInfo.item_name .. ")")
-		end
+		local item = {
+			name = lineInfo.item_name,
+			id = lineInfo.item_id,
+		}
+		-- TODO: Find a way to get the player's items list or to check if the player has a specific item
+		--if (player.inventory:HasItem(ItemManager.GetItem(item.id))) then
+		--	ModUtils:Log("Item: " .. itemID .. " (" .. lineInfo.item_name .. ")")
+		--end
 	end
 end
 System.AddCCommand(ModMain.prefix .. 'DumpPlayerInventory', 'ModInventory:DumpPlayerInventory()', "Dumps the player's inventory to the log")

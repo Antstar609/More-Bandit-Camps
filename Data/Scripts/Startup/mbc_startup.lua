f3831ae44f8c8028f787feccb9c341755d180e48
Antstar609 <antstar666@gmail.com>
2024-04-14 16:48:43 +0200

    Renamed files prefix to match the name of the mod

---
+++ Data/Scripts/Startup/mbc_startup.lua
@@ -0,0 +1,8 @@
+-- Reload all scripts
+Script.ReloadScript("scripts/mbc_main.lua")
+Script.ReloadScript("scripts/mbc_utils.lua")
+Script.ReloadScript("scripts/mbc_quest.lua")
+Script.ReloadScript("scripts/mbc_soul.lua")
+Script.ReloadScript("scripts/mbc_database.lua")
+Script.ReloadScript("scripts/mbc_camps.lua")
+Script.ReloadScript("scripts/mbc_inventory.lua")
--- Data/Scripts/Startup/mod_startup.lua
+++
@@ -1,8 +0,0 @@
--- Reload all scripts
-Script.ReloadScript("scripts/mod_main.lua")
-Script.ReloadScript("scripts/mod_utils.lua")
-Script.ReloadScript("scripts/mod_quest.lua")
-Script.ReloadScript("scripts/mod_soul.lua")
-Script.ReloadScript("scripts/mod_database.lua")
-Script.ReloadScript("scripts/mod_camps.lua")
-Script.ReloadScript("scripts/mod_inventory.lua")
